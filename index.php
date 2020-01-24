a6435e27e0894de0a864596d055e7088a1ffc754
Oleg Rovinskiy <orovinskiy@mail.greenriver.edu>
2020-01-24 13:35:18 -0500

    Defined the routes and displayed the final message

--- index.php
+++ index.php
@@ -20,10 +20,24 @@ $f3->route('GET /', function () {
     echo $view->render('views/home.html');
 });
 
-//define a default route
+//route for form one
+$f3->route('GET /order', function () {
+    $view = new Template();
+    echo $view->render('views/form1.html');
+});
+
+//route for form 2
 $f3->route('POST /order2', function () {
+    $_SESSION['animal'] = $_POST['animal'];
     $view = new Template();
     echo $view->render('views/form2.html');
+});
+
+//route for results page
+$f3->route('POST /results', function () {
+    $_SESSION['color'] = $_POST['color'];
+    $view = new Template();
+    echo $view->render('views/results.html');
 });
 
 
